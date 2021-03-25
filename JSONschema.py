17653120f3a60747461769bf8e49f1af7fda0a70
kdubeygit <79244642+kdubeygit@users.noreply.github.com>
2021-03-25 16:32:55 +0530

    Create JSONschema.py
    Creating JSON Schema

---
+++ JSONschema.py
@@ -0,0 +1,40 @@
+import python_jsonschema_objects as pjs
+import json
+
+schema = '''{
+    "title": "Example Schema",
+    "type": "object",
+    "properties": {
+        "firstName": {
+            "type": "string"
+        },
+        "lastName": {
+            "type": "string"
+        },
+        "age": {
+            "description": "Age in years",
+            "type": "integer",
+            "minimum": 0
+        },
+        "dogs": {
+            "type": "array",
+            "items": {"type": "string"},
+            "maxItems": 4
+        },
+        "gender": {
+            "type": "string",
+            "enum": ["male", "female"]
+        },
+        "deceased": {
+            "enum": ["yes", "no", 1, 0, "true", "false"]
+            }
+    },
+    "required": ["firstName", "lastName"]
+} '''
+
+schema = json.loads(schema)
+builder = pjs.ObjectBuilder(schema)
+ns = builder.build_classes()
+Person = ns.ExampleSchema
+james = Person(firstName="James", lastName="Bond")
+print(james.firstName)
